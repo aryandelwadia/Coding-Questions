105f4c9d29a36a802878854446cec876f1ba4e6a
Aryan Delawadia <aryandelwadia@gmail.com>
2024-09-02 15:03:56 +0530

    binary tree

---
+++ Coding Ninja/Day 17 Count Leaf Nodes.cpp
@@ -0,0 +1,44 @@
+//link
+//https://www.naukri.com/code360/problems/count-leaf-nodes_893055
+
+/**********************************************************
+
+    Following is the Binary Tree Node class structure:
+
+    template <typename T>
+    class BinaryTreeNode {
+      public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+                this -> data = data;
+                left = NULL;
+                right = NULL;
+        }
+    };
+    
+***********************************************************/
+
+void inOrderTraversal(BinaryTreeNode<int> *root, int &count){
+    if (root == NULL){
+        return;
+    }
+
+    inOrderTraversal(root -> left, count);
+    if(root -> left == NULL && root -> right == NULL){
+        count++;
+    }
+    inOrderTraversal(root -> right, count);
+}
+
+int noOfLeafNodes(BinaryTreeNode<int> *root){
+    // Write your code here.
+
+    int count = 0;
+
+    inOrderTraversal(root, count);
+
+    return count;
+}
