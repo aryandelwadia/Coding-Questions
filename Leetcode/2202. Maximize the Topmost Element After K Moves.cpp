9103ad153f164e4ac4e167582a455ee9eca5707b
Aryan Delawadia <aryandelwadia@gmail.com>
2025-08-11 23:59:22 +0530

    daily

---
+++ Leetcode/2202. Maximize the Topmost Element After K Moves.cpp
@@ -0,0 +1,26 @@
+//link
+//https://leetcode.com/problems/maximize-the-topmost-element-after-k-moves/
+
+class Solution {
+public:
+    int maximumTop(vector<int>& nums, int k) {
+        if(k == 0) return nums[0];
+        if (nums.size() == 1) {
+            return k % 2 == 0 ? nums[0] : -1;
+        }
+
+        if (k == 1) return nums[1];
+        if (k > nums.size()) {
+            return *max_element(nums.begin(), nums.end());
+        }
+
+        int maxi = INT_MIN;
+        for (int i = 0; i < k - 1; ++i) {
+            maxi = max(maxi, nums[i]);
+        }
+        if (k < nums.size()) {
+            maxi = max(maxi, nums[k]);
+        }
+        return maxi;
+    }
+};
